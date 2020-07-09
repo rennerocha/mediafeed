537f29b2a5cba45329682884a6925bb670075e7e
Renne Rocha <renne@rennerocha.com>
2020-07-09 15:12:00 -0300

    Improve test of managers methods to retrieve videos

--- yt_organizer/channels/managers.py
+++ yt_organizer/channels/managers.py
@@ -6,22 +6,12 @@ from django.utils import timezone
 
 class VideoQuerySet(models.QuerySet):
     def last_24h(self):
-        from channels.models import Video
-
-        start_datetime = timezone.now().replace(microsecond=0) - datetime.timedelta(
-            hours=24
-        )
-        return Video.objects.filter(published_date__gte=start_datetime)
+        start_datetime = timezone.now() - datetime.timedelta(hours=24, minutes=1)
+        return self.filter(published_date__gte=start_datetime)
 
     def last_week(self):
-        from channels.models import Video
-
-        start_datetime = timezone.now().replace(microsecond=0) - datetime.timedelta(
-            days=7
-        )
-        return Video.objects.filter(published_date__gte=start_datetime)
+        start_datetime = timezone.now() - datetime.timedelta(days=7, minutes=1)
+        return self.filter(published_date__gte=start_datetime)
 
     def for_categories(self, categories):
-        from channels.models import Video
-
-        return Video.objects.filter(channel__categories__in=categories)
+        return self.filter(channel__categories__in=categories)
--- yt_organizer/channels/tests/test_managers.py
+++ yt_organizer/channels/tests/test_managers.py
@@ -11,7 +11,7 @@ class VideoManagerTestCase(TestCase):
     def setUp(self):
         self.channel = baker.make(Channel)
 
-    def test_last_24h(self):
+    def test_last_24h_videos(self):
         just_now = timezone.now()
         last_24h = just_now - datetime.timedelta(hours=24)
         older_than_24h = last_24h - datetime.timedelta(minutes=1)
@@ -32,7 +32,7 @@ class VideoManagerTestCase(TestCase):
         self.assertTrue(video_last_24h in videos)
         self.assertTrue(video_older_than_24h not in videos)
 
-    def test_last_week(self):
+    def test_last_week_videos(self):
         last_week = timezone.now() - datetime.timedelta(days=7)
         older_than_week = last_week - datetime.timedelta(minutes=1)
 
@@ -44,7 +44,7 @@ class VideoManagerTestCase(TestCase):
         self.assertTrue(video_last_week in videos)
         self.assertTrue(older_than_week_video not in videos)
 
-    def test_videos_for_categories(self):
+    def test_for_categories_videos(self):
         category_1 = baker.make(Category)
         channel_1 = baker.make(Channel)
         video_1 = baker.make(Video, channel=channel_1)
@@ -69,9 +69,10 @@ class VideoManagerTestCase(TestCase):
         self.assertTrue(video_2 in videos)
         self.assertTrue(video_3 not in videos)
 
-    def test_videos_last_24h_videos_for_categories(self):
+    def test_last_24h_for_categories_videos(self):
         category_1 = baker.make(Category)
         channel_1 = baker.make(Channel)
+        category_1.channels.add(channel_1)
 
         last_24h = timezone.now() - datetime.timedelta(hours=24)
         older_than_24h = last_24h - datetime.timedelta(minutes=1)
@@ -81,17 +82,37 @@ class VideoManagerTestCase(TestCase):
         video_3 = baker.make(Video, channel=channel_1, published_date=older_than_24h)
         video_4 = baker.make(Video, channel=channel_1, published_date=older_than_24h)
 
-        categories = Category.objects.filter(title__in=[category_1.title])
-        videos = Video.objects.for_categories(categories).last_24h()
+        videos = Video.objects.last_24h().for_categories([category_1])
 
         self.assertTrue(video_1 in videos)
         self.assertTrue(video_2 in videos)
         self.assertTrue(video_3 not in videos)
         self.assertTrue(video_4 not in videos)
 
-    def test_videos_last_week_videos_for_categories(self):
+    def test_for_categories_last_24h_videos(self):
         category_1 = baker.make(Category)
         channel_1 = baker.make(Channel)
+        category_1.channels.add(channel_1)
+
+        last_24h = timezone.now() - datetime.timedelta(hours=24)
+        older_than_24h = last_24h - datetime.timedelta(minutes=1)
+
+        video_1 = baker.make(Video, channel=channel_1, published_date=last_24h)
+        video_2 = baker.make(Video, channel=channel_1, published_date=last_24h)
+        video_3 = baker.make(Video, channel=channel_1, published_date=older_than_24h)
+        video_4 = baker.make(Video, channel=channel_1, published_date=older_than_24h)
+
+        videos = Video.objects.for_categories([category_1]).last_24h()
+
+        self.assertTrue(video_1 in videos)
+        self.assertTrue(video_2 in videos)
+        self.assertTrue(video_3 not in videos)
+        self.assertTrue(video_4 not in videos)
+
+    def test_last_week_for_categories_videos(self):
+        category_1 = baker.make(Category)
+        channel_1 = baker.make(Channel)
+        category_1.channels.add(channel_1)
 
         last_week = timezone.now() - datetime.timedelta(days=7)
         older_than_week = last_week - datetime.timedelta(minutes=1)
@@ -101,8 +122,27 @@ class VideoManagerTestCase(TestCase):
         video_3 = baker.make(Video, channel=channel_1, published_date=older_than_week)
         video_4 = baker.make(Video, channel=channel_1, published_date=older_than_week)
 
-        categories = Category.objects.filter(title__in=[category_1.title])
-        videos = Video.objects.for_categories(categories).last_week()
+        videos = Video.objects.last_week().for_categories([category_1])
+
+        self.assertTrue(video_1 in videos)
+        self.assertTrue(video_2 in videos)
+        self.assertTrue(video_3 not in videos)
+        self.assertTrue(video_4 not in videos)
+
+    def test_for_categories_last_week_videos(self):
+        category_1 = baker.make(Category)
+        channel_1 = baker.make(Channel)
+        category_1.channels.add(channel_1)
+
+        last_week = timezone.now() - datetime.timedelta(days=7)
+        older_than_week = last_week - datetime.timedelta(minutes=1)
+
+        video_1 = baker.make(Video, channel=channel_1, published_date=last_week)
+        video_2 = baker.make(Video, channel=channel_1, published_date=last_week)
+        video_3 = baker.make(Video, channel=channel_1, published_date=older_than_week)
+        video_4 = baker.make(Video, channel=channel_1, published_date=older_than_week)
+
+        videos = Video.objects.for_categories([category_1]).last_week()
 
         self.assertTrue(video_1 in videos)
         self.assertTrue(video_2 in videos)
