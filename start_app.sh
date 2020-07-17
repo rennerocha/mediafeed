ed2bea92490afc40d12fb352b64a43bbe7ec03a9
Renne Rocha <renne@rennerocha.com>
2020-07-17 11:08:05 -0300

    Start cron service when starting application to make scheduled tasks to be executed

--- start_app.sh
+++ start_app.sh
@@ -5,5 +5,6 @@ printenv | grep -v "no_proxy" >> /etc/environment
 python manage.py crontab add
 python manage.py collectstatic --noinput
 python manage.py migrate
+cron
 
 gunicorn --workers=2 --bind=0.0.0.0:8000 yt_organizer.wsgi:application
