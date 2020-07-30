02cd4330179f967866e1375419345b4ad02da8b9
Renne Rocha <renne@rennerocha.com>
2020-07-30 15:34:01 -0300

    Remove duplicated test case

--- mediafeed/channels/tests/test_views.py
+++ mediafeed/channels/tests/test_views.py
@@ -70,14 +70,6 @@ class CategoryDetailAccessTestCase(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
-    def test_access_public_category_without_authenticated(self):
-        url = reverse(
-            "channels:category_details",
-            args=(self.user.username, self.public_category.slug,),
-        )
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-
     def test_access_public_category_of_another_user(self):
         other_user = User.objects.create_user(
             "otheruser", "otheruser@test.com", "otheruserpassword"
